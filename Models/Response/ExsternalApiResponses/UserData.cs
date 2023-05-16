43d8cddd88ef1ec274ca40f8991b5c812db41c6b
NatanGer97 <42692104+NatanGer97@users.noreply.github.com>
2023-05-16 20:53:01 +0300

    with user data model

---
+++ Models/Response/ExsternalApiResponses/UserData.cs
@@ -0,0 +1,11 @@
+﻿using MemoGlobal_BackendHomeTest.Models.Entity;
+using Newtonsoft.Json;
+
+namespace MemoGlobal_BackendHomeTest.Models.Response.ExsternalApiResponses
+{
+    public class UserData
+    {
+        [JsonProperty("data")]
+        public User Data { get; set; }
+    }
+}
