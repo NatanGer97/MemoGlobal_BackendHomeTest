d483b01a7d90aa4663b66adf48eac4f96a6faa93
NatanGer97 <42692104+NatanGer97@users.noreply.github.com>
2023-05-16 20:52:40 +0300

    add json property to user model

--- Models/Entity/User.cs
+++ Models/Entity/User.cs
@@ -1,14 +1,25 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace MemoGlobal_BackendHomeTest.Models.Entity
 {
-    
+
     public class User
     {
+        [JsonProperty("id")]
         public int Id { get; set; }
+
+        [JsonProperty("email")]
         public string Email { get; set; }
+
+        [JsonProperty("first_name")]
         public string FirstName { get; set; }
+
+        [JsonProperty("last_name")]
         public string LastName { get; set; }
+
+        [JsonProperty("avatar")]
         public string Avatar { get; set; }
     }
 }
