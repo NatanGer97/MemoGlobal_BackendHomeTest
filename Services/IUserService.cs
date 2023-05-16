fbc3c43b14b1718a59eee9a8ae89480b20a1e533
NatanGer97 <42692104+NatanGer97@users.noreply.github.com>
2023-05-16 19:10:45 +0300

    with IUserService Interface

---
+++ Services/IUserService.cs
@@ -0,0 +1,15 @@
+﻿using MemoGlobal_BackendHomeTest.Models.DTO;
+using MemoGlobal_BackendHomeTest.Models.Entity;
+
+namespace MemoGlobal_BackendHomeTest.Services
+{
+    public interface IUserService
+    {
+        public Task<List<User>> ReadUserFromPage(int page);
+        public Task<User> ReadUser(int id);
+        public Task<User> CreateUser(CreateUserRequest createUserRequest);
+
+        public Task DeleteUser(int id);
+        public Task UpdateUser(int id, CreateUserRequest createUserRequest);
+    }
+}
