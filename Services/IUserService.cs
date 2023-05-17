20ff5f07a17566d0344833387263aa34fe694c3a
NatanGer97 <42692104+NatanGer97@users.noreply.github.com>
2023-05-17 10:52:51 +0300

    change return type to user or users response in IUserService

--- Services/IUserService.cs
+++ Services/IUserService.cs
@@ -1,15 +1,17 @@
 ﻿using MemoGlobal_BackendHomeTest.Models.DTO;
 using MemoGlobal_BackendHomeTest.Models.Entity;
+using MemoGlobal_BackendHomeTest.Models.Response;
 
 namespace MemoGlobal_BackendHomeTest.Services
 {
     public interface IUserService
     {
-        public Task<List<User>?> ReadUserFromPage(int page);
-        public Task<User?> ReadUser(int id);
-        public Task<User?> CreateUser(CreateUserRequest createUserRequest);
+        public Task<UsersResponse> ReadUserFromPage(int page);
+        public Task<UserResponse> CreateUser(CreateUserRequest createUserRequest);
 
-        public Task DeleteUser(int id);
-        public Task UpdateUser(int id, CreateUserRequest createUserRequest);
+        public Task<UserResponse> DeleteUser(int id);
+        public Task<UserResponse> UpdateUser(int id, CreateUserRequest createUserRequest);
+
+        public Task<UserResponse> GetUserById(int userId);
     }
 }
