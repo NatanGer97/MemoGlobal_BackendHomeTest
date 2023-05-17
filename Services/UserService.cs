289c6f8b74a841be38c0692151f142da71a1e47a
NatanGer97 <42692104+NatanGer97@users.noreply.github.com>
2023-05-17 10:53:33 +0300

    change return type to user or users response in UserService and add implementation to Delete and Update methods

--- Services/UserService.cs
+++ Services/UserService.cs
@@ -1,5 +1,6 @@
 ﻿using MemoGlobal_BackendHomeTest.Models.DTO;
 using MemoGlobal_BackendHomeTest.Models.Entity;
+using MemoGlobal_BackendHomeTest.Models.Response;
 using MemoGlobal_BackendHomeTest.Models.Response.ExsternalApiResponses;
 using Newtonsoft.Json;
 
@@ -16,37 +17,110 @@ namespace MemoGlobal_BackendHomeTest.Services
 
         private readonly string baseUrl = "https://reqres.in/api/users";
 
-        public async Task<User?> CreateUser(CreateUserRequest createUserRequest)
+        public async Task<UserResponse> CreateUser(CreateUserRequest createUserRequest)
         {
-            User? responseUser = null;
+            UserResponse badUserResponse = new UserResponse("error while creating user");
             var response = await httpClient.PostAsJsonAsync(baseUrl, createUserRequest);
             if (response.IsSuccessStatusCode)
             {
                 string responseAsString = await response.Content.ReadAsStringAsync();
-                
+
                 User? user = JsonConvert.DeserializeObject<User>(responseAsString);
-                
+
                 if (user != null)
                 {
-                    responseUser = user;
+                    return new UserResponse(user);
                 }
-                
+                return badUserResponse;
+
                 // TODO: save to db               
             }
 
-            return responseUser;
+            return badUserResponse;
         }
 
-        public Task DeleteUser(int id)
+        public async Task<UserResponse> DeleteUser(int id)
         {
-            throw new NotImplementedException();
+            // first check if user exist
+            UserResponse userResponse = await GetUserById(id);
+            if (!userResponse.IsSuccess)
+            {
+                return userResponse;
+            }
+
+            string url = baseUrl + $"/{id}";
+            HttpResponseMessage response = await httpClient.DeleteAsync(url);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return userResponse;
+            }
+
+            else
+            {
+                return new UserResponse("error while deleting user");
+            }
+
+
+
         }
 
-        public async Task<User?> ReadUser(int id)
+
+        public async Task<UsersResponse> ReadUserFromPage(int page)
+        {
+            string url = baseUrl + $"?page={page}";
+            UsersResponse badResponse = new UsersResponse("bad response");
+            var response = await httpClient.GetAsync(url);
+            
+            if (response.IsSuccessStatusCode)
+            {
+                string responseAsString = await response.Content.ReadAsStringAsync();
+                ListOfUserData? listOfUserData = JsonConvert.DeserializeObject<ListOfUserData>(responseAsString);
+                if (listOfUserData != null)
+                {
+                    return new UsersResponse(listOfUserData.usersList);
+                }
+                
+
+                return badResponse;
+
+            }
+
+            return badResponse;
+        }
+
+        public async Task<UserResponse> UpdateUser(int id, CreateUserRequest createUserRequest)
         {
             string url = baseUrl + $"/{id}";
+            UserResponse userResponse = await GetUserById(id);
+            if (!userResponse.IsSuccess)
+            {
+                return userResponse;
+            }
+
+            var response = await httpClient.PutAsJsonAsync<CreateUserRequest>(url, createUserRequest);
+
+
+            if (response.IsSuccessStatusCode)
+            {
+                var responseAsString = await response.Content.ReadAsStringAsync();
+                User? user = JsonConvert.DeserializeObject<User>(responseAsString);
+
+                if (user != null)
+                {
+                    return new UserResponse(user);
+                }
+
+                return new UserResponse("updating user gone wrong");
+            }
+
+            return new UserResponse("updating user gone wrong");
+        }
+
+        public async Task<UserResponse> GetUserById(int userId)
+        {
+            string url = baseUrl + $"/{userId}";
             var response = await httpClient.GetAsync(url);
-            User? user = null;
 
             if (response.IsSuccessStatusCode)
             {
@@ -55,36 +129,20 @@ namespace MemoGlobal_BackendHomeTest.Services
 
                 if (userData != null)
                 {
-                    user = userData.user;
+                    return new UserResponse(userData.user);
                 }
 
+                return new UserResponse(new User());
+
             }
-
-            return user;
-        }
-
-        public async Task<List<User>?> ReadUserFromPage(int page)
-        {
-            string url = baseUrl + $"?page={page}";
-            List<User>? results = null;
-
-            var response = await httpClient.GetAsync(url);
-            if (response.IsSuccessStatusCode)
+            else
             {
-                string responseAsString = await response.Content.ReadAsStringAsync();
-                ListOfUserData? listOfUserData = JsonConvert.DeserializeObject<ListOfUserData>(responseAsString);
-                 if (listOfUserData != null)
-                {
-                    results = listOfUserData.usersList;
-                }
+                return new UserResponse("user not found");
             }
 
-            return results;
-        }
 
-        public Task UpdateUser(int id, CreateUserRequest createUserRequest)
-        {
-            throw new NotImplementedException();
+
+
         }
     }
 }
