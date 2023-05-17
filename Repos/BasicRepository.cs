770564d6e51864f9feb6ad04f2a088789fb7899b
NatanGer97 <42692104+NatanGer97@users.noreply.github.com>
2023-05-17 12:59:07 +0300

    with user repository and basic repository

---
+++ Repos/BasicRepository.cs
@@ -0,0 +1,14 @@
+﻿using MemoGlobal_BackendHomeTest.DBContexts;
+
+namespace MemoGlobal_BackendHomeTest.Repos
+{
+    public abstract class BasicRepository
+    {
+        protected UsersContext Context { get; private set; }
+
+        public BasicRepository(UsersContext context)
+        {
+            Context = context;
+        }
+    }
+}
---
+++ Repos/IUserRepo.cs
@@ -0,0 +1,12 @@
+﻿using MemoGlobal_BackendHomeTest.Models.Entity;
+
+namespace MemoGlobal_BackendHomeTest.Repos
+{
+    public interface IUserRepo
+    {
+        Task AddUser(User user);
+        Task AddUsers(List<User> users);
+        void Update(User newUser, User oldUser);
+        void Delete(User user);
+    }
+}
